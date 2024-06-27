344c15259a952dde8aff32279acd3336942b1e47
Staffan Åberg <staffan.aaberg@gmail.com>
2024-06-27 15:06:21 +0200

    My first commit

---
+++ src/test/java/calculator/MyClassTest.java
@@ -0,0 +1,15 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MyClassTest {
+    @Test
+    public void testAdd() {
+        MyClass myClass = new MyClass();
+        assertEquals(3, myClass.add(1, 2));
+        assertEquals(0, myClass.add(-1, 1));
+        assertEquals(0, myClass.add(0, 0));
+    }
+}
